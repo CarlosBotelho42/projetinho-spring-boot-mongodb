898700ef18676bb461df09c87862489989f69a04
Carlos Henrique Botelho Nascimento <carloshbn@LNTB-MUL040517.network.ctbc>
2020-02-14 13:51:01 -0300

    primiero commit

---
+++ src/main/java/Projetinho/SBMDB/ProjetinhoSBMDB/ProjetinhoSbmdbApplication.java
@@ -0,0 +1,13 @@
+package Projetinho.SBMDB.ProjetinhoSBMDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetinhoSbmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetinhoSbmdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projetinho/SBMDB/ProjetinhoSBMDB/ProjetinhoSbmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package Projetinho.SBMDB.ProjetinhoSBMDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetinhoSbmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
